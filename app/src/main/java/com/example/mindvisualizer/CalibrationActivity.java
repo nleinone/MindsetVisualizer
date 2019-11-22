e83138277f51a174edf015fcff82949978248924
Maeva <maevaespana@orange.fr>
2019-11-22 20:39:30 +0200

    Few code changes + new button
    Added a button to return to the ConnectionActivity instead of being redirected automatically after the completion of the calibration.
    
    Working on displaying an alertDialog to notify the user that the calibration was successful.

--- app/src/main/java/com/example/mindvisualizer/CalibrationActivity.java
+++ app/src/main/java/com/example/mindvisualizer/CalibrationActivity.java
@@ -2,18 +2,34 @@ package com.example.mindvisualizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class CalibrationActivity extends AppCompatActivity implements View.OnClickListener {
 
     private boolean calibrated = false;
+    private AlertDialog success;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibration);
+        findViewById(R.id.start_calib).setOnClickListener(CalibrationActivity.this);
+        findViewById(R.id.return_prev).setOnClickListener(CalibrationActivity.this);
+
+        final AlertDialog success = new AlertDialog.Builder(CalibrationActivity.this).create();
+        success.setTitle(getString(R.string.calibSuccessT));
+        success.setMessage(getString(R.string.calibSuccessM));
+        success.setButton(DialogInterface.BUTTON_NEUTRAL, getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                success.cancel();
+            }
+        });
     }
 
     @Override
@@ -29,11 +45,14 @@ public class CalibrationActivity extends AppCompatActivity implements View.OnCli
              */
 
             calibrated = true;
-            if(calibrated) {
+            success.show();
+        }
+        if (v.getId() == R.id.return_prev) {
+            //if(calibrated) { //might not need this condition in the final code because the user might have a malfunctioning headband, therefore wanting to disconnect from this one and connect to another one
                 Intent i = new Intent(CalibrationActivity.this, ConnectionActivity.class);
                 i.putExtra("calib", calibrated);
                 startActivity(i);
-            }
+            //}
         }
     }
 }

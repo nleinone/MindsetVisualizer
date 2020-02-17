5e0270d2725d23bb897867fa3b5de649a168829d
nleinone <nikoleino91@gmail.com>
2020-02-17 15:13:28 +0200

    Fixed average calculation bug. Added finish operation in visualActivity when pausing or stopping activity

--- app/src/main/java/com/example/mindvisualizer/VisualActivity.java
+++ app/src/main/java/com/example/mindvisualizer/VisualActivity.java
@@ -25,14 +25,16 @@ public class VisualActivity extends AppCompatActivity {
 
     private static final String TAG = "VisualActivity";
 
-    public float loadDataFromFirebase(String sessionId)
+
+    public void loadDataFromFirebase(String sessionId)
     {
-        Log.v("VisualActivity","load 1");
+        Log.v("test","load 1");
         //Get firebase database reference:
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference firebaseRootReference = database.getReference("musefirebase-79096");
         DatabaseReference sessionReference = firebaseRootReference.child("Session-" + sessionId);
-        Log.v("VisualActivity","load 2");
+        Log.v("test","load 2");
+
         sessionReference.addListenerForSingleValueEvent(
                 new ValueEventListener() {
                     @Override
@@ -42,45 +44,51 @@ public class VisualActivity extends AppCompatActivity {
                         // Result will be holded Here
                         for (DataSnapshot dsp : dataSnapshot.getChildren()) {
                             String strValue = String.valueOf(dsp.getValue());
-                            Log.v("VisualActivity", "eegValue: " + dsp.getValue());
-                            Log.v("VisualActivity", "eegValueStr: " + strValue);
+                            Log.v("test", "eegValuetest: " + dsp.getValue());
+                            Log.v("test", "eegValueStr: " + strValue);
 
                             dataList.add(String.valueOf(dsp.getValue())); //add result into array list
 
                         }
+
                         int len = dataList.size();
-                        Log.v("VisualActivity", "lenDataList func: " + len);
-                        Log.v("VisualActivity","load 3");
+                        Log.v("test", "lenDataList func: " + len);
+                        Log.v("test","load 3");
 
                         SharedPreferences prefsAvg = getApplicationContext().getSharedPreferences("prefsAvg", 0);
                         float avgEEG = getAvg(dataList);
+                        Log.v("test", "avgEEG: " + String.valueOf(avgEEG));
                         prefsAvg.edit().putString("avgEEG", String.valueOf(avgEEG)).apply();
                     }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
                         //handle databaseError
-                        Log.v("VisualActivity", "Error in handling Firebase data");
+                        Log.v("test", "Error in handling Firebase data");
                     }
                 });
-        SharedPreferences prefsAvg = getApplicationContext().getSharedPreferences("prefsAvg", 0);
-        String avgEEG = prefsAvg.getString("avgEEG", "");
-        float avgEEGFloat = Float.parseFloat(avgEEG);
-        Log.v("VisualActivity", "Load function result: " + avgEEGFloat);
-        return avgEEGFloat;
+
     }
+
 
     private float getAvg(ArrayList<String> dataList)
     {
-        float avgEEG = 0;
+        float sum = 0;
         float floatNum = 0;
 
-        for (String i : dataList)
-            floatNum = Float.parseFloat(i);
-            avgEEG = avgEEG + floatNum;
-            String stringEEG = String.valueOf(floatNum);
-            Log.v("getAvg", "avgEEGString: " + stringEEG);
+        int len = dataList.size();
+        Log.v("getAvg", "lenDataList func: " + len);
 
+        for (String i : dataList) {
+            floatNum = Float.parseFloat(i);
+            Log.v("getAvg", "floatNum: " + floatNum);
+            sum = sum + floatNum;
+            Log.v("getAvg", "avgEEG: " + sum);
+            String stringEEG = String.valueOf(floatNum);
+            Log.v("getAvg", "stringEEG: " + stringEEG);
+        }
+        Log.v("getAvg", "avgEEGReturn: " + sum);
+        float avgEEG = sum / len;
         return avgEEG;
     }
 
@@ -88,12 +96,16 @@ public class VisualActivity extends AppCompatActivity {
     {
         //get current sessionId:
         SharedPreferences prefCalibrationMode = getApplicationContext().getSharedPreferences("prefCalibrationMode", 0); // 0 - for private mode
-        String sessionId = prefCalibrationMode.getString("SessionId", "0");
         String calibMode = prefCalibrationMode.getString("CalibrationMode", "0");
+
+        SharedPreferences prefsAvg = getApplicationContext().getSharedPreferences("prefsAvg", 0);
+        String avgEEG = prefsAvg.getString("avgEEG", "");
+        Log.v("VisualActivity", "avgEEG2: " + String.valueOf(avgEEG));
+        float avgEEGFloat = Float.parseFloat(avgEEG);
+        Log.v("VisualActivity", "Load function result: " + avgEEGFloat);
 
         if(calibMode.equals("2"))
         {
-            float avgEEG = loadDataFromFirebase(sessionId);
 
             String avgEEGString = String.valueOf(avgEEG);
             Log.v("VisualActivity", "avgEEGString: " + avgEEGString);
@@ -114,38 +126,38 @@ public class VisualActivity extends AppCompatActivity {
 
             int direction = -1;
 
-            if(color_change_variable >= avgEEG) {
+            if(color_change_variable >= avgEEGFloat) {
 
                 RL.setBackgroundColor(Color.parseColor("#69FF40"));
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.10) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.10) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#8FFF40"));
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.15) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.15) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#A9FF40") );
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.20) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.20) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#C6FF40"));
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.25) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.25) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#E3FF40"));
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.30) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.30) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#FFFC40"));
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.35) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.35) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#FFDC40"));
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.40) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.40) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#FFBF40"));
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.45) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.45) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#FF9940"));
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.50) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.50) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#FF7440"));
             }
-            else if(color_change_variable >= avgEEG + (avgEEG * 0.55) * (direction)){
+            else if(color_change_variable >= avgEEGFloat + (avgEEGFloat * 0.55) * (direction)){
                 RL.setBackgroundColor(Color.parseColor("#FF5D40"));
             }
             else
@@ -159,9 +171,32 @@ public class VisualActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visual);
+        //Calculate threshold value:
+        SharedPreferences prefCalibrationMode = getApplicationContext().getSharedPreferences("prefCalibrationMode", 0); // 0 - for private mode
+        String sessionId = prefCalibrationMode.getString("SessionId", "0");
+        String calibMode = prefCalibrationMode.getString("CalibrationMode", "0");
+
+        if(calibMode.equals("2"))
+        {
+            loadDataFromFirebase(sessionId);
+        }
+
         SharedPreferences prefEeg = getApplicationContext().getSharedPreferences("avgEEG", 0);
         prefEeg.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener);
 
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        finish();
+    }
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        finish();
     }
     //Do stuff when key is changed
     SharedPreferences.OnSharedPreferenceChangeListener sharedPreferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
